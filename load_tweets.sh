e9fac87a87473f59079151b3c42661faf92eef37
gibsonfriedman <gibsonafriedman@gmail.com>
2024-04-04 00:01:18 -0700

    please work

--- load_tweets.sh
+++ load_tweets.sh
@@ -9,10 +9,10 @@ test-data.zip
 
 echo 'load normalized'
 for file in $files; do
-	python3 load_tweets.py --db=postgresql://postgres:pass@localhost:1358 --inputs "$file"
+	time python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:112258 --inputs "$file"
 done
 
 echo 'load denormalized'
 for file in $files; do
-	unzip -f "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1357 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+	time unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:112259 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
